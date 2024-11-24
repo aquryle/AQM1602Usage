903868c0af6e458f0937e693146c6cd1f379f72b
aquryle <aquryle@gmail.com>
2024-11-25 01:16:43 +0900

    initial commit

---
+++ Core/Inc/aqm1602.h
@@ -0,0 +1,13 @@
+/*
+ * aqm1602.h
+ *
+ *  Created on: Nov 25, 2024
+ *      Author: sata
+ */
+
+#ifndef INC_AQM1602_H_
+#define INC_AQM1602_H_
+
+
+
+#endif /* INC_AQM1602_H_ */
---
+++ Core/Src/aqm1602.c
@@ -0,0 +1,8 @@
+/*
+ * aqm1602.c
+ *
+ *  Created on: Nov 25, 2024
+ *      Author: sata
+ */
+
+
